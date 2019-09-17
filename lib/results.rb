18c22b966c8b5333e6c5fe9d7c9b2219ee517ba2
Guy Calloway <[guyfcalloway@gmail.com]>
2019-09-17 20:56:05 +0100

    1st Green, 2nd Red

---
+++ lib/results.rb
@@ -0,0 +1,3 @@
+def results(string)
+    return ('Range: 00|00|00 Average: 00|00|00 Median: 00|00|00')
+end
--- spec/results_spec.rb
+++ spec/results_spec.rb
@@ -1,7 +1,11 @@
 require 'results'
 
-describe "statsMachine" do
+describe "results" do
     it "Returns 'Range: 00|00|00 Average: 00|00|00 Median: 00|00|00' when passed '00|00|00'" do
-        expect(results('00|00|00').to eq('Range: 00|00|00 Average: 00|00|00 Median: 00|00|00'))
+        expect(results('00|00|00')).to eq('Range: 00|00|00 Average: 00|00|00 Median: 00|00|00')
+    end
+
+    it "Returns '' when passed an empty string" do
+        expect(results('')).to eq('')
     end
 end
